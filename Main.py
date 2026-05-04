39effb47ba4240a25b7aaf14edb581226c1384de
ShauryaChawla <sxc1713@student.bham.ac.uk>
2026-05-04 21:22:35 +0100

    Completed - Visualizing Video

--- Main.py
+++ Main.py
@@ -2,13 +2,30 @@
 import cv2
 import os
 
-# reading image
-image_path = os.path.join('.', 'data', 'test-img.png')
-img = cv2.imread(image_path)
+# Reading Video
+video_path = os.path.join('.', 'data', 'vid.mp4')
+video = cv2.VideoCapture(video_path)
 
-# write image
-cv2.imwrite(os.path.join('.', 'data', 'test-img-out.png'), img)
 
-# visualizing image
-cv2.imshow('image', img)
-cv2.waitKey(0)  # NEED THIS ELSE WINDOW CLOSES IMMEDIATELY
+# Visualizing Video
+ret = True
+while ret:
+    ret, frame = video.read()
+
+    # Displaying the frame
+    cv2.imshow('video', frame)
+    cv2.waitKey(33)
+
+video.release()
+cv2.destroyAllWindows()
+
+# # reading image
+# image_path = os.path.join('.', 'data', 'test-img.png')
+# img = cv2.imread(image_path)
+
+# # write image
+# cv2.imwrite(os.path.join('.', 'data', 'test-img-out.png'), img)
+
+# # visualizing image
+# cv2.imshow('image', img)
+# cv2.waitKey(0)  # NEED THIS ELSE WINDOW CLOSES IMMEDIATELY
