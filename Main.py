44c31e25fa1fe774c0b4ca8776ece46f98bae301
ShauryaChawla <sxc1713@student.bham.ac.uk>
2026-05-04 21:05:10 +0100

    Visualizing Image

--- Main.py
+++ Main.py
@@ -2,8 +2,13 @@
 import cv2
 import os
 
-# READING IMAGE
+# reading image
 image_path = os.path.join('.', 'data', 'bird.jpg')
 img = cv2.imread(image_path)
 
 # write image
+cv2.imwrite(os.path.join('.', 'data', 'bird_out.jpg'), img)
+
+# visualizing image
+cv2.imshow('image', img)
+cv2.waitKey(0)  # NEED THIS ELSE WINDOW CLOSES IMMEDIATELY
